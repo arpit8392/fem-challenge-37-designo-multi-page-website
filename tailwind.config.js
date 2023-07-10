ce48d000fe579e1b7c15ed2f556b248f1637fb7e
Arpit Namdev <arpit8392@gmail.com>
2023-07-10 19:14:00 +0530

    Initial Setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				peach: '#E7816B',
+				offBlack: '#1D1C1E',
+				lightPeach: '#FFAD9B',
+				darkGrey: '#333136',
+				lightGrey: '#F1F3F5',
+			},
+		},
+	},
+	plugins: [require('@tailwindcss/forms')],
 }
